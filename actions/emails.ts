dc9c14fdb5c466b5a115864b5f694b0ca3ff22e1
Themba Dlamini <49640058+Tembanator@users.noreply.github.com>
2026-04-08 20:20:05 +0200

    Update emails.ts

--- actions/emails.ts
+++ actions/emails.ts
@@ -31,7 +31,7 @@ import { NewUserNotificationEmail } from "@/emails/NewUserNotificationEmail";
 // });
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
+  port: 465,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
